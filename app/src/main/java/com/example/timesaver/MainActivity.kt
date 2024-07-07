f3cf698f4f15579a7fbc51182ff73a3700cdae65
AngelynDisguise <angelyn.domingo122@gmail.com>
2024-07-07 15:41:08 -0400

    Add toolbar and configure drawer destinations

--- app/src/main/java/com/example/timesaver/MainActivity.kt
+++ app/src/main/java/com/example/timesaver/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.timesaver
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Set up ActionBar
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph, drawerLayout))
+
+        // Set up NavigationView
+        navView.setupWithNavController(navController)
     }
+
+    // Override onSupportNavigateUp to handle the Up button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(AppBarConfiguration(navController.graph, drawerLayout)) || super.onSupportNavigateUp()
+    }
+
 }
