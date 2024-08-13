d5bd9dfae3738a5653111facb3ee28dd25f3f9a2
AngelynDisguise <angelyn.domingo122@gmail.com>
2024-08-13 12:49:29 -0400

    feat(LogsFragment): implement updating timelogs
    - need to add settings for date format, time format, and chronological order (newest-oldest/oldest-newest)

--- app/src/main/java/com/example/timesaver/fragments/activity/LogsFragment.kt
+++ app/src/main/java/com/example/timesaver/fragments/activity/LogsFragment.kt
@@ -1,22 +1,44 @@
 package com.example.timesaver.fragments.activity
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.timesaver.MainActivity
 import com.example.timesaver.MainViewModel
 import com.example.timesaver.R
 import com.example.timesaver.database.Timelog
+import com.google.android.material.snackbar.Snackbar
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class LogsFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: TimelogListAdapter
+    private val calendar = Calendar.getInstance()
+
+    // Sort
+    private var sortByNewest = true
+
+    // Filter
+    // TODO(): By today, daily, weekly, monthly?
+
+    // Formats
+    private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy")
+    private val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm a")
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -79,20 +101,193 @@ class LogsFragment : Fragment() {
             )
         }
 
-        adapter.setOnClickTimelogListener { vh ->
-            // TODO(): Save input UI?
-            adapter.toggleChildRow(vh)
+        adapter.setOnClickTimelogListener { viewHolder ->
+            adapter.toggleChildRow(viewHolder) // input UI is saved while fragment is alive
         }
 
-        adapter.setOnClickDeleteListener { v, t ->
-            // TODO() Delete timelog
+        adapter.setOnClickDeleteListener { deleteView, timelog ->
+            deleteTimelog(deleteView, timelog)
         }
 
-        adapter.setOnClickConfirmListener { vh, t ->
-            // TODO() Update timelog
-            adapter.toggleChildRow(vh)
+        adapter.setOnClickConfirmListener { viewHolder, timelog ->
+            if (validateEditInput(viewHolder, timelog)) {
+                adapter.toggleChildRow(viewHolder)
+            }
+        }
+
+        adapter.setOnEditDateListener {
+            openDatePicker(it)
+        }
+
+        adapter.setOnEditStartTimeListener { start, end, se, ee ->
+            openTimePicker(start, end, se, ee,true)
+        }
+
+        adapter.setOnEditEndTimeListener { start, end, se, ee ->
+            openTimePicker(start, end, se, ee, false)
         }
     }
 
+    private fun openDatePicker(dateEditText: EditText) {
+        DatePickerDialog(
+            requireContext(),
+            { _, year, month, dayOfMonth ->
+                calendar.set(year, month, dayOfMonth)
+                val dateFormat = SimpleDateFormat("MM-dd-yyyy", Locale.US)
+                dateEditText.setText(dateFormat.format(calendar.time))
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+
+    private fun openTimePicker(
+        startTime: LocalTime,
+        endTime: LocalTime,
+        startEditText: EditText,
+        endEditText: EditText,
+        isStart: Boolean
+    ) {
+        var currStartTime: LocalTime = startTime
+        var currEndTime: LocalTime = endTime
+
+        // Open Time picker
+        TimePickerDialog(
+            requireContext(),
+            { _, hourOfDay, minute ->
+                // Take new time and put it on the edit text
+                val time: LocalTime = LocalTime.of(hourOfDay, minute)
+                val currEditText: EditText = if (isStart) startEditText else endEditText
+                currEditText.setText(time.format(timeFormatter))
+
+                // Update start and time values for comparison
+                if (isStart) {
+                    currStartTime = time // start was changed
+                    if (endEditText.text.isNotEmpty()) { // both start and end are changed
+                        currEndTime = timeFormatter.parse(endEditText.text, LocalTime::from)
+                    }
+                } else {
+                    currEndTime = time // end was changed
+                    if (startEditText.text.isNotEmpty()) { // both start and end are changed
+                        currStartTime = timeFormatter.parse(startEditText.text, LocalTime::from)
+                    }
+                }
+
+                // Validate time range and reject if invalid
+                if (isStart && currStartTime.isAfter(currEndTime)) {
+                    Toast.makeText(requireContext(), "Start time cannot be later than end time. Try again.", Toast.LENGTH_LONG).show()
+                    currEditText.text.clear()
+                } else if (!isStart && currEndTime.isBefore(currStartTime)) {
+                    Toast.makeText(requireContext(), "End time cannot be earlier than start time. Try again.", Toast.LENGTH_LONG).show()
+                    currEditText.text.clear()
+                }
+            },
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            false
+        ).show()
+    }
+
+    private fun validateEditInput(viewHolder: TimelogListAdapter.ViewHolder, timelog: Timelog): Boolean {
+        val dateText = viewHolder.dateEditText.text
+        val startText = viewHolder.startTimeEditText.text
+        val endText = viewHolder.endTimeEditText.text
+
+        val currDate = if (dateText.isNotEmpty()) dateFormatter.parse(dateText, LocalDate::from) else timelog.date
+        val currStartTime = if (startText.isNotEmpty()) timeFormatter.parse(startText, LocalTime::from) else timelog.startTime
+        val currEndTime = if (endText.isNotEmpty()) timeFormatter.parse(endText, LocalTime::from) else timelog.endTime
+
+        if (dateText.isNotEmpty() || startText.isNotEmpty() || endText.isNotEmpty()) {
+            val newTimelog = Timelog(
+                timelogId = timelog.timelogId,
+                activityId = timelog.activityId,
+                date = currDate,
+                startTime = currStartTime,
+                endTime = currEndTime
+            )
+            return insertNewTimelog(adapter.currentList, newTimelog)
+        }
+        return false
+    }
+
+    private fun insertNewTimelog(timelogs: List<Timelog>, newTimelog: Timelog): Boolean {
+        val sortedTimelogs: List<Timelog> = sortTimelogs(timelogs, sortByNewest)
+        val overlappingTimelog = sortedTimelogs
+            .firstOrNull { it.timelogId != newTimelog.timelogId && it.date == newTimelog.date && it.overlaps(newTimelog) }
+
+        Log.d(
+            "LogsFragment",
+            "Overlapping timelog = $overlappingTimelog"
+        )
+
+        return overlappingTimelog?.let {
+            Toast.makeText(requireContext(), "Time range overlaps with another log on that date. " +
+                    "Please try again.", Toast.LENGTH_LONG).show()
+            Log.d(
+                "LogsFragment",
+                "Time range (${newTimelog.startTime.format(timeFormatter)} - ${newTimelog.endTime.format(timeFormatter)}) overlaps with another time log on that date (${newTimelog.date})."
+            )
+            false
+        } ?: let {
+            val timelogExists: Boolean = timelogs.any { it.timelogId == newTimelog.timelogId }
+            if (timelogExists) {
+                viewModel.updateTimelog(newTimelog)
+            } else {
+                viewModel.addTimelog(newTimelog)
+            }
+            true
+        }
+    }
+
+    private fun sortTimelogs(timelogs: List<Timelog>, byNewest: Boolean): List<Timelog> {
+        return when {
+            timelogs.isEmpty() -> emptyList()
+            byNewest -> timelogs.sortedWith(compareByDescending<Timelog> { it.date }
+                .thenBy { it.startTime })
+            else -> timelogs.sortedWith(compareBy<Timelog> { it.date } // ascending
+                .thenBy { it.startTime })
+        }
+    }
+
+    private fun deleteTimelog(view: View, timelog: Timelog) {
+        // Confirm and provide Undo option
+        val oldList = adapter.currentList
+        val newList = adapter.currentList.toMutableList()
+
+        val removed: Boolean = newList.remove(timelog)
+        require(removed) { "Tried to remove a timelog that doesn't exist." }
+
+        viewModel.deleteTimelog(timelog)
+
+        Log.i(
+            "LogsFragment",
+            "Deleted Timelog: $timelog"
+        )
+
+        Snackbar.make(view, "Timelog for ${timelog.date.format(dateFormatter)} deleted", Snackbar.LENGTH_LONG)
+            .setAction("UNDO") {
+                viewModel.addTimelog(timelog)
+                Log.i(
+                    "LogsFragment",
+                    "Delete undone"
+                )
+            }
+            .show()
+    }
+
+    private fun updateTimelog(timelog: Timelog) {
+        val newList = adapter.currentList.toMutableList()
+        val i = newList.indexOfFirst { timelog.timelogId == it.timelogId }
+        newList[i] = timelog
+
+        adapter.submitList(newList)
+        viewModel.updateTimelog(timelog)
+
+        Log.i(
+            "LogsFragment",
+            "Updated Timelog: $timelog"
+        )
+    }
 
 }
--- app/src/main/java/com/example/timesaver/fragments/activity/TimelogListAdapter.kt
+++ app/src/main/java/com/example/timesaver/fragments/activity/TimelogListAdapter.kt
@@ -6,6 +6,7 @@ import android.animation.ValueAnimator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -14,12 +15,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.timesaver.R
 import com.example.timesaver.database.Timelog
 import java.time.Duration
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class TimelogListAdapter: ListAdapter<Timelog, TimelogListAdapter.ViewHolder>(TimelogDiffCallback()) {
 
+    // Parent row clickables
     private var onClickTimelogListener: ((ViewHolder) -> Unit)? = null
     private var onClickDeleteListener: ((View, Timelog) -> Unit)? = null
+
+    // Child row clickables
+    private var onEditDateListener: ((EditText) -> Unit)? = null
+    private var onEditStartTimeListener: ((LocalTime, LocalTime, EditText, EditText) -> Unit)? = null
+    private var onEditEndTimeListener: ((LocalTime, LocalTime, EditText, EditText) -> Unit)? = null
     private var onClickConfirmListener: ((ViewHolder, Timelog) -> Unit)? = null
 
     private var dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy") // default
@@ -29,11 +37,18 @@ class TimelogListAdapter: ListAdapter<Timelog, TimelogListAdapter.ViewHolder>(Ti
         val parentRow: LinearLayout = view.findViewById(R.id.timelog_parent_row_layout)
         val childRow: LinearLayout = view.findViewById(R.id.timelog_child_row_layout)
 
+        // Parent stuff
         val dateTextView: TextView = view.findViewById(R.id.timelog_date_text_view)
         val totalTimeTextView: TextView = view.findViewById(R.id.timelog_total_time_text_view)
         val startTimeTextView: TextView = view.findViewById(R.id.timelog_start_time_text_view)
         val endTimeTextView: TextView = view.findViewById(R.id.timelog_end_time_text_view)
 
+        // Child stuff
+        val dateEditText: EditText = view.findViewById(R.id.timelog_date_edit_text_view)
+        val startTimeEditText: EditText = view.findViewById(R.id.timelog_start_time_edit_text_view)
+        val endTimeEditText: EditText = view.findViewById(R.id.timelog_end_time_edit_text_view)
+
+        // Icons
         val deleteIcon: ImageView = view.findViewById(R.id.timelog_delete_icon)
         val confirmIcon: ImageView = view.findViewById(R.id.timelog_confirm_icon)
     }
@@ -53,17 +68,26 @@ class TimelogListAdapter: ListAdapter<Timelog, TimelogListAdapter.ViewHolder>(Ti
         viewHolder.endTimeTextView.text = timelog.endTime.format(timeFormat)
         viewHolder.totalTimeTextView.text = formatDuration(Duration.between(timelog.startTime, timelog.endTime))
 
-
-
-        // Set up clickables
+        // Set up parent clickables
         viewHolder.parentRow.setOnClickListener {
             onClickTimelogListener?.invoke(viewHolder)
         }
         viewHolder.deleteIcon.setOnClickListener {
             onClickDeleteListener?.invoke(it, timelog)
         }
+
+        // Set up child clickables
         viewHolder.confirmIcon.setOnClickListener {
             onClickConfirmListener?.invoke(viewHolder, timelog)
+        }
+        viewHolder.dateEditText.setOnClickListener {
+            onEditDateListener?.invoke(viewHolder.dateEditText)
+        }
+        viewHolder.startTimeEditText.setOnClickListener {
+            onEditStartTimeListener?.invoke(timelog.startTime, timelog.endTime, viewHolder.startTimeEditText, viewHolder.endTimeEditText)
+        }
+        viewHolder.endTimeEditText.setOnClickListener {
+            onEditEndTimeListener?.invoke(timelog.startTime, timelog.endTime, viewHolder.startTimeEditText, viewHolder.endTimeEditText)
         }
     }
 
@@ -139,6 +163,19 @@ class TimelogListAdapter: ListAdapter<Timelog, TimelogListAdapter.ViewHolder>(Ti
     fun setOnClickDeleteListener(listener: (View, Timelog) -> Unit) {
         onClickDeleteListener = listener
     }
+
+    fun setOnEditDateListener(listener: (EditText) -> Unit) {
+        onEditDateListener = listener
+    }
+
+    fun setOnEditStartTimeListener(listener: (LocalTime, LocalTime, EditText, EditText) -> Unit) {
+        onEditStartTimeListener = listener
+    }
+
+    fun setOnEditEndTimeListener(listener: (LocalTime, LocalTime, EditText, EditText) -> Unit) {
+        onEditEndTimeListener = listener
+    }
+
 
     fun setOnClickConfirmListener(listener: (ViewHolder, Timelog) -> Unit) {
         onClickConfirmListener = listener
